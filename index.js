5db99051875f682eb623e7262fbaa0c30223ca57
samuel villada <samuelvillada555@gmail.com>
2026-06-25 06:45:43 -0500

    cambio de puerto

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
-'#crud clientes'
+
 app.get('/clientes', clienteController.listar)
 app.get('/clientes/:correo', clienteController.consultarId)
 app.post('/clientes', clienteController.registrar)
@@ -15,15 +15,16 @@ app.put('/clientes/:correo', clienteController.actualizar)
 app.delete('/clientes/:correo', clienteController.eliminar)
 app.get('/registrar', clienteController.formulario)
 
-'#crud servicios'
+
 app.get('/servicios', servicioController.listar);
 app.get('/servicios/:nombre', servicioController.buscarID);
 app.post('/servicios', servicioController.registrar)
 app.put('/servicios/:nombre', servicioController.actualizar)
 app.delete('/servicios/:nombre', servicioController.eliminar);
 
+const PORT = process.env.PORT || 9999;
 
-app.listen(9999, ()=>{
+app.listen(PORT, ()=>{
     console.log('Aplicacion en linea')
 })
 
